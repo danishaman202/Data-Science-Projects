241145b3e0f3c5e90f7a83d1667be37fb9bc1034
danishaman202 <89190312+danishaman202@users.noreply.github.com>
2021-09-09 16:49:59 +0500

    Add files via upload

---
+++ Recommender system/Recommender data 4.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 01:36:20 2021
+
+@author: danish
+"""
+# Movie recommender by user rating
+import pandas as pd 
+from sklearn.decomposition import TruncatedSVD
+import numpy as np
+column = ['user_id','item_id','rating','timestamp']
+data = pd.read_csv('C:/Users/danish/Documents/ml-100k/u.data', sep = "\t", names = column)
+column2 = ['item_id','movie name','release_date', 'video release date','imdb URL', 'unknown', 'action','adventure','animation', 'children', 'comedy', 'crime', 'documentary', 'drama', 'fantasy', 'film noir', 'horror', 'musical', 'mystery', 'romance', 'scifi', 'thriller', 'war', 'western']
+movie = pd.read_csv('C:/Users/danish/Documents/ml-100k/u.item', sep = "|", encoding='latin-1', names = column2)
+data_2 = pd.merge(data,movie, on = 'item_id')
+crosstab = data_2.pivot_table(values = 'rating', columns = 'movie name', index = 'user_id', fill_value = 0)
+X = crosstab.T.values
+SVD = TruncatedSVD(n_components = 12, random_state = 17)
+result_m = SVD.fit_transform(X)
+corrcof = np.corrcoef(result_m)
+m = crosstab.columns
+m = list(m)
+
+recommender = pd.DataFrame(corrcof,columns = m,index = m)
+def recommend(name):
+    X = pd.Series(recommender[name].nlargest(5).index)
+    X.drop(0,inplace = True)
+    return X
+def namer():
+    while True:
+        name = input('Enter an Integer to respond to your movie:')
+        if name.isdigit():
+            name = int(name)
+            if name < 1665:
+                print('Your movie name is ',m[name],".....\n")
+                name = m[name]
+            else:
+                print('Please enter integer less than 1664')
+                continue
+            break
+        else:
+            print('Please enter a valid digit')
+    print('The movies that correlate most with your movies are\n')
+    return recommend(name)
+print(namer())
+
+      
